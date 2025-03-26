d9b31ba06a443e0e63a9f3ec97bd0fa8b9d138b2
Max Sponner <max.sponner@outlook.com>
2025-03-26 21:37:26 +0100

    I hate pyproject.toml

---
+++ notdienst_finder/__init__.py
@@ -0,0 +1,3 @@
+from .pharmacy import Pharmacy, get_emergency_pharmacies  # or any function, class, or variable from pharmacy.py
+
+__all__ = ["Pharmacy", "get_emergency_pharmacies"]  # Makes `Pharmacy` accessible when importing `notdienst_finder`
